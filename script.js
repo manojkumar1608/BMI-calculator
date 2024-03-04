4b99565ae5c28e83d495e41b211bea871203552a
manoj <pitlamanoj1@gmail.com>
2024-03-04 18:54:35 +0530

    completed

---
+++ script.js
@@ -0,0 +1,33 @@
+  const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+  
+
+  if (height === '' || height < 0 || isNaN(height)){
+    results.innerHTML = `Please enter a valid Height ${height}`;
+  }
+  else if (weight === '' || weight < 0 || isNaN(weight)){
+    results.innerHTML = `Please enter a valid Weight ${weight}`;
+  }
+  else{
+    const bmi = (weight/((height*height)/10000)).toFixed(2)
+    results.innerHTML= `<span> ${bmi} </span>`
+  }
+    const bmi = (weight/((height*height)/10000)).toFixed(2)
+if(bmi < 18.6){
+  results.innerHTML =`${bmi}underweight`
+}
+else if(bmi === 18.6 || bmi < 25){
+  results.innerHTML =`${bmi}Normal weight`
+}
+else if(bmi > 24.9){
+  results.innerHTML =`${bmi}Overweight`
+}
+
+    
+  })
+
